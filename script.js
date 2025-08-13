6047beb164d7e564be29dc88b6b9bfa82156d428
Mateo <mateobinance1@gmail.com>
2025-08-12 21:23:20 -0300

    changes

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function convertToBoolean (value) {
     return Boolean(value);
 }
 
+if (typeof window !== 'undefined') {
+    window.sum = sum;
+    window.formatMoney = formatMoney;
+    window.convertToBoolean = convertToBoolean;
+} else if (typeof global !== 'undefined') {
+    global.sum = sum;
+    global.formatMoney = formatMoney;
+    global.convertToBoolean = convertToBoolean;
+}
